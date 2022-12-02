3f01f38ceca1ce7607eaba3cfbfe761a470dc071
SierraEcoXray <105768778+SierraEcoXray@users.noreply.github.com>
2022-12-02 22:29:15 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -41,44 +41,44 @@ placeholder = st.empty()
 df = df[df["module"] == module_filter]
 
 # near real-time / live feed simulation
-# for seconds in range(200):
+ for seconds in range(200):
 
-#     df["age_new"] = df["age"] * np.random.choice(range(1, 5))
-#     df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
+     df["age_new"] = df["age"] * np.random.choice(range(1, 5))
+     df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
 
-#     # creating KPIs
-#     avg_age = np.mean(df["age_new"])
+     # creating KPIs
+     avg_age = np.mean(df["age_new"])
 
-#     count_married = int(
-#         df[(df["marital"] == "married")]["marital"].count()
-#         + np.random.choice(range(1, 30))
-#     )
+     count_married = int(
+         df[(df["marital"] == "married")]["marital"].count()
+         + np.random.choice(range(1, 30))
+     )
 
-#     balance = np.mean(df["balance_new"])
+     balance = np.mean(df["balance_new"])
 
-#     with placeholder.container():
+     with placeholder.container():
 
-#         # create three columns
-#         kpi1, kpi2, kpi3 = st.columns(3)
+         # create three columns
+         kpi1, kpi2, kpi3 = st.columns(3)
 
-#         # fill in those three columns with respective metrics or KPIs
-#         kpi1.metric(
-#             label="Age ⏳",
-#             value=round(avg_age),
-#             delta=round(avg_age) - 10,
-#         )
-        
-#         kpi2.metric(
-#             label="Married Count 💍",
-#             value=int(count_married),
-#             delta=-10 + count_married,
-#         )
-        
-#         kpi3.metric(
-#             label="A/C Balance ＄",
-#             value=f"$ {round(balance,2)} ",
-#             delta=-round(balance / count_married) * 100,
-#         )
+         # fill in those three columns with respective metrics or KPIs
+         kpi1.metric(
+             label="Age ⏳",
+             value=round(avg_age),
+             delta=round(avg_age) - 10,
+         )
+       
+         kpi2.metric(
+             label="Married Count 💍",
+             value=int(count_married),
+             delta=-10 + count_married,
+         )
+       
+         kpi3.metric(
+             label="A/C Balance ＄",
+             value=f"$ {round(balance,2)} ",
+             delta=-round(balance / count_married) * 100,
+         )
 
         # create two columns for charts
 fig_col1, fig_col2 = st.columns(2)
@@ -98,7 +98,7 @@ with fig_col2:
     time.sleep(1)
 
 
-# In[ ]:
+ In[ ]:
 
 
 
