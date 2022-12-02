b21c11e686ef7b620736896cd722fbf2bdbc0929
SierraEcoXray <105768778+SierraEcoXray@users.noreply.github.com>
2022-12-02 22:32:58 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -29,75 +29,59 @@ def get_data() -> pd.DataFrame:
 df = get_data()
 
 # dashboard title
-st.title("Real-Time / KNU Xbee Data Analysis")
+st.title("Real-Time / Live Data Science Dashboard")
 
-# top-level filters
-module_filter = st.selectbox("Select the module", pd.unique(df["module"]))
+# top-level filters 
 
-# creating a single-element container
+job_filter = st.selectbox("Select the Job", pd.unique(df['job']))
+
+
+# creating a single-element container.
 placeholder = st.empty()
 
-# dataframe filter
-df = df[df["module"] == module_filter]
+# dataframe filter 
 
-# near real-time / live feed simulation
+df = df[df['job']==job_filter]
+
+# near real-time / live feed simulation 
+
 for seconds in range(200):
+#while True: 
+    
+    df['age_new'] = df['age'] * np.random.choice(range(1,5))
+    df['balance_new'] = df['balance'] * np.random.choice(range(1,5))
 
-     df["age_new"] = df["age"] * np.random.choice(range(1, 5))
-     df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
+    # creating KPIs 
+    avg_age = np.mean(df['age_new']) 
 
-     # creating KPIs
-     avg_age = np.mean(df["age_new"])
+    count_married = int(df[(df["marital"]=='married')]['marital'].count() + np.random.choice(range(1,30)))
+    
+    balance = np.mean(df['balance_new'])
 
-     count_married = int(
-         df[(df["marital"] == "married")]["marital"].count()
-         + np.random.choice(range(1, 30))
-     )
+    with placeholder.container():
+        # create three columns
+        kpi1, kpi2, kpi3 = st.columns(3)
 
-     balance = np.mean(df["balance_new"])
+        # fill in those three columns with respective metrics or KPIs 
+        kpi1.metric(label="Age ⏳", value=round(avg_age), delta= round(avg_age) - 10)
+        kpi2.metric(label="Married Count 💍", value= int(count_married), delta= - 10 + count_married)
+        kpi3.metric(label="A/C Balance ＄", value= f"$ {round(balance,2)} ", delta= - round(balance/count_married) * 100)
 
-     with placeholder.container():
+        # create two columns for charts 
 
-         # create three columns
-         kpi1, kpi2, kpi3 = st.columns(3)
-
-         # fill in those three columns with respective metrics or KPIs
-         kpi1.metric(
-             label="Age ⏳",
-             value=round(avg_age),
-             delta=round(avg_age) - 10,
-         )
-       
-         kpi2.metric(
-             label="Married Count 💍",
-             value=int(count_married),
-             delta=-10 + count_married,
-         )
-       
-         kpi3.metric(
-             label="A/C Balance ＄",
-             value=f"$ {round(balance,2)} ",
-             delta=-round(balance / count_married) * 100,
-         )
-
-        # create two columns for charts
-fig_col1, fig_col2 = st.columns(2)
-with fig_col1:
-    st.markdown("### First Chart")
-    fig = px.density_heatmap(
-        data_frame=df, y="module", x="amount"
-            )
-    st.write(fig)
-with fig_col2:
-    st.markdown("### Second Chart")
-    fig2 = px.histogram(data_frame=df, x="amount")
-    st.write(fig2)
-
-    st.markdown("### Detailed Data View")
-    st.dataframe(df)
-    time.sleep(1)
-
-# In[ ]:
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("### First Chart")
+            fig = px.density_heatmap(data_frame=df, y = 'age_new', x = 'marital')
+            st.write(fig)
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame = df, x = 'age_new')
+            st.write(fig2)
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
+    #placeholder.empty()
 
 
 
