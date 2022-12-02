bf570bd389a213fd306e004d93ef70d23991f122
SierraEcoXray <105768778+SierraEcoXray@users.noreply.github.com>
2022-12-02 22:29:40 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -41,7 +41,7 @@ placeholder = st.empty()
 df = df[df["module"] == module_filter]
 
 # near real-time / live feed simulation
- for seconds in range(200):
+for seconds in range(200):
 
      df["age_new"] = df["age"] * np.random.choice(range(1, 5))
      df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
